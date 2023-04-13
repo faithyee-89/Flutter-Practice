a1628804707fc77cf8ec5d5d5f10fb71a0fed3ea
faith_yee <11196649@qq.com>
2023-04-13 10:18:47 +0800

    1. 更新Lib 2. 更新练习代码

--- flutter_lesson_2_5/lib/main.dart
+++ flutter_lesson_2_5/lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyAppV2());
+}
+
+class MyAppV2 extends StatelessWidget {
+  const MyAppV2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            width: 552,
+            height: 85,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xFFABBDD5)),
+            child: Text(
+              "登录",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 32),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -11,58 +38,55 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 文本控件演示代码
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          // 文本控件演示代码
           Container(
             margin: EdgeInsets.only(top: 100),
-              height: 200,
-              width: 200,
-              color: Colors.blue,
-              // 文本控件
-              child: Text(
-                '文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本',
-                /**
+            height: 200,
+            width: 200,
+            color: Colors.blue,
+            // 文本控件
+            child: Text(
+              '文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本',
+              /**
              * 文本对齐方式
              * TextAlign.end
              */
-                textAlign: TextAlign.end,
-                overflow: TextOverflow.clip,
-                maxLines: 2,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w100,
-                  fontStyle: FontStyle.italic,
-                  letterSpacing: 5,
-                  height: 2,
-                  decoration: TextDecoration.overline,
-                ),
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.clip,
+              maxLines: 3,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.italic,
+                letterSpacing: 5,
+                height: 2,
+                decoration: TextDecoration.overline,
               ),
             ),
-            // Image控件演示代码
-            Container(
-              width: 300,
-              height: 300,
-              color: Colors.orange,
-              // 网络图片地址：https://doc.flutterchina.club/images/flutter-mark-square-100.png
-              // child: Image.asset(
-              //   'image/flutter_icon.jpg',
-              //   // 填充方式
-              //   fit: BoxFit.fill,
-              // ),
+          ),
+          // Image控件演示代码
+          Container(
+            width: 300,
+            height: 300,
+            color: Colors.orange,
+            // 网络图片地址：https://doc.flutterchina.club/images/flutter-mark-square-100.png
+            // child: Image.asset(
+            //   'image/flutter_icon.jpg',
+            //   // 填充方式
+            //   fit: BoxFit.fill,
+            // ),
 
-              // child: Image.network(
-              //   'https://doc.flutterchina.club/images/flutter-mark-square-100.png',
-              //   fit: BoxFit.fill,
-              // ),
-
-              child: Icon(
-                Icons.stars,
-                ),
-
+            child: Image.network(
+              'https://doc.flutterchina.club/images/flutter-mark-square-100.png',
+              fit: BoxFit.fill,
             ),
+
+            // child: Icon(
+            //   Icons.stars,
+            // ),
+          ),
         ]),
       ),
     );
